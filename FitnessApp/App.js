edfdaeaad8b2e1c2ef58deecc47679cfbf889460
Zekton12 <k1917111@kingston.ac.uk>
2023-03-23 21:28:46 +0000

    HomeScreen Commit

--- FitnessApp/App.js
+++ FitnessApp/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fitness App from me!!!</Text>
+      <HomeScreen/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ FitnessApp/components/FitnessCards.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FitnessCards = () => {
+  return (
+    <View>
+      <Text>FitnessCards</Text>
+    </View>
+  )
+}
+
+export default FitnessCards
+
+const styles = StyleSheet.create({})
