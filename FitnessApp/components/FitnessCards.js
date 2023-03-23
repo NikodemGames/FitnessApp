85de911eaa5ac9d506230020593ab4ddac7adabe
Zekton12 <k1917111@kingston.ac.uk>
2023-03-23 22:35:53 +0000

    HomeScreen Design Finished

--- FitnessApp/components/FitnessCards.js
+++ FitnessApp/components/FitnessCards.js
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import fitness from '../data/fitness'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const FitnessCards = () => {
+    const FitnessData = fitness;
   return (
     <View>
-      <Text>FitnessCards</Text>
+        {FitnessData.map((item, key) => (
+            <Pressable style={{alignItems:"center", justifyContent:"center",margin:10}} key={key}>
+                <Image style={{width:"90%", height:140,borderRadius:7}} source={{uri:item.image}}>
+
+                </Image>
+                <Text style={{position:"absolute", color:"white", fontSize:16,fontWeight:"bold",left:20,top:20}}>{item.name}</Text>
+                <MaterialCommunityIcons style={{position:"absolute", color:"white",bottom:15, left:20}} name="lightning-bolt" size={24} color="black" />
+            </Pressable>
+        ))}
     </View>
   )
 }
--- FitnessApp/screens/HomeScreen.js
+++ FitnessApp/screens/HomeScreen.js
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View, SafeAreaView, Image, } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from 'react-native'
 import React from 'react'
+import FitnessCards from '../components/FitnessCards'
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView>
+        <ScrollView style={{marginTop:50}}>
             <View style={{ backgroundColor: "#CD853F", padding: 10, height: 200, width: "100%" }}>
                 <Text style={{ color: "white", fontWeight: "bold", fontSize: 18 }}>HOME WORKOUT</Text>
                 <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", marginTop: 20 }}>
@@ -25,8 +26,9 @@ const HomeScreen = () => {
                 <View style={{justifyContent: "center",alignItems:"center"}}>
                     <Image style={{width:"90%",height:120,marginTop:20,borderRadius:7}} source={{uri:"https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_842,ar_1.2,q_auto:eco,dpr_2,f_auto,fl_progressive/image/test/sku-card-widget/gold2.png",}}/>
                 </View>
+                <FitnessCards/>
             </View>
-        </SafeAreaView>
+        </ScrollView>
     )
 }
 
