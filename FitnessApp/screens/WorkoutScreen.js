8050d921917748c79a08ea312e070bedd35423f8
Alejandro Vaughan <k1843224@kingston.ac.uk>
2023-03-24 16:59:11 +0000

    Improved Navigation
    Added more pressable functionality for each button on home screen and a back button to return to home screen

--- FitnessApp/screens/WorkoutScreen.js
+++ FitnessApp/screens/WorkoutScreen.js
@@ -1,16 +1,23 @@
-import { StyleSheet, Text, View, SafeAreaView, Image} from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, Pressable} from 'react-native'
 import React from 'react'
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons';
 
 const WorkoutScreen = () => {
     const route = useRoute();
     console.log(route.params)
+    const navigation = useNavigation();
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{backgroundColor:"white"}}>
       <Image style={{width: '100%', height: 170}} source={{uri:route.params.image}}/>
+
+      <Entypo onPress={() => navigation.goBack()}
+      style={{position:"absolute", top:50, left:20}} name="back" size={28} color="white" />
+
+      
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default WorkoutScreen
 
